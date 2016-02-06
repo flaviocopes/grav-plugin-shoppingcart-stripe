ef02f3144f9654a62e1e5cb87b5ec0db6fff3641
Flavio Copes <copesc@gmail.com>
2016-02-06 17:42:14 +0100

    First commit

---
+++ shoppingcart-stripe.php
@@ -0,0 +1,82 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use Grav\Common\Uri;
+use RocketTheme\Toolbox\Event\Event;
+
+class ShoppingcartStripePlugin extends Plugin
+{
+    protected $plugin_name = 'shoppingcart-stripe';
+
+    protected $gateway;
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     */
+    public function onTwigSiteVariables()
+    {
+        $this->grav['assets']->addJs('plugin://' . $this->plugin_name . '/gateways/stripe/script.js');
+    }
+
+    /**
+     * Enable search only if url matches to the configuration.
+     */
+    public function onPluginsInitialized()
+    {
+        require_once __DIR__ . '/vendor/autoload.php';
+
+        $this->config->set('plugins.shoppingcart', array_replace_recursive($this->config->get('plugins.shoppingcart'), $this->config->get('plugins.shoppingcart-stripe')));
+
+        if (!$this->isAdmin()) {
+            // Site
+            $this->enable([
+                'onTwigSiteVariables'          => ['onTwigSiteVariables', 0],
+                'onShoppingCartPay'            => ['onShoppingCartPay', 0],
+            ]);
+        }
+    }
+
+    /**
+     *
+     */
+    protected function requireGateway()
+    {
+        $path = realpath(__DIR__ . '/../shoppingcart/classes/gateway.php');
+        if (!file_exists($path)) {
+            $path = realpath(__DIR__ . '/../grav-plugin-shoppingcart/classes/gateway.php');
+        }
+        require_once($path);
+    }
+
+    /**
+     *
+     */
+    public function getGateway()
+    {
+        if (!$this->gateway) {
+            $this->requireGateway();
+            require_once __DIR__ . '/gateways/stripe/gateway.php';
+            $this->gateway = new ShoppingCartGatewayStripe();
+        }
+
+        return $this->gateway;
+    }
+
+    /**
+     * @param $event
+     */
+    public function onShoppingCartPay($event)
+    {
+        $this->getGateway()->onShoppingCartPay($event);
+    }
+}
